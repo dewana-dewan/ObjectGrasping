11c3a6aa8518e31cd45c128aa8fd747d50a00aaf
Aditya Dewan <iit2015097@iiita.ac.in>
2017-11-12 23:09:11 +0530

    fuck fuzzy mode

--- gyawali_histogram.py
+++ gyawali_histogram.py
@@ -1,13 +1,14 @@
 import scipy
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import ndimage
 
 
 # 0. Read the image
-image  = scipy.misc.imread('pcd0312r.png',mode="L")
+image  = scipy.misc.imread('samples/pcd0312r.png',mode="L")
 img_arr = np.array(image)
-print image
+print (image)
 # 1. Get the histogram of the iamge
 cn = []
 
@@ -15,8 +16,8 @@ for i in range(256) :
     x = float(i+1)/2
     cn.append(x)
 
-print len(cn)
-print img_arr.shape[0]
+print (len(cn))
+print (img_arr.shape[0], img_arr.shape[0])
 
 for i in range (img_arr.shape[0]) :
     for j in range (img_arr.shape[1]) :
@@ -27,29 +28,52 @@ bn = []
 for i in range(256):
     bn.append(float(i))
 
-
-#--------
-
-#   fhist ,fedges =     np.histogram2d(x,y,cn,normed=False,weights=None)
-        
-#---
-
 hist, bin_edges = np.histogram(img_arr, bins=cn)
 bin_centers = 0.5*(bin_edges[:-1] + bin_edges[1:])
 
-# 2. Get the min, max, mean, value of the image
-'''im_min = image.min()
-im_max = image.max()
-im_mean = image.mean()
+print(bin_centers)
 
-print "Min : ",im_min
-print "Max : ",im_max
-print "Mean : ",im_mean
-'''
-#array = np.array(hist)
-print hist
+bins_arr = []
+bc = []
+bin_size = 30
+
+for i in range(int(255/bin_size) + 1):
+	print(i)
+	bins_arr.append(0)
+	bc.append(i * bin_size)
+
+print(bins_arr)
+
+for i in range(img_arr.shape[0]):
+	for j in range(img_arr.shape[1]):
+		
+		a = int(img_arr[i][j] / bin_size)
+		b = img_arr[i][j] % bin_size
+		k = abs(0.5 - b / bin_size)
+		# round(k, 3)
+		# print(k)
+
+		if (b <= bin_size / 2):
+			if (a == 0):
+				bins_arr[0] += 1
+			else:
+				bins_arr[a] += 1 - k
+				bins_arr[a - 1] += k
+		else:
+			bins_arr[a] += 1 - k
+			bins_arr[a + 1] += k
+			
+c = 0
+for i in range(int(255/bin_size) + 1):
+	c += bins_arr[i]
+
+print(bins_arr, c)
+
+# print (hist)
 
 # 3. Plot the histogram
-import matplotlib.pyplot as plt
-plt.plot(bin_centers, hist, lw=2)
+# import matplotlib.pyplot as plt
+#plt.plot(bin_centers, hist, lw=2)
+
+plt.plot(bc, bins_arr, lw=2)
 plt.show()
