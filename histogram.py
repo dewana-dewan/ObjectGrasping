b73cd8dd252b411c69e18de9ce8646ca99df8c87
Aditya Dewan <iit2015097@iiita.ac.in>
2017-11-19 01:52:50 +0530

    verifying histograms correctness

--- histogram.py
+++ histogram.py
@@ -4,41 +4,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import ndimage
 
-image  = scipy.misc.imread('sample_results/glass.png',mode="L")
+image  = scipy.misc.imread('./BTOW.jpg',mode="L")
 img_arr = np.array(image)
 print (image)
 # 1. Get the histogram of the iamge
-cn = []
+# cn = []
 
-for i in range(256) :
-    x = float(i+1)/2
-    cn.append(x)
+# for i in range(256) :
+#     x = float(i+1)/2
+#     cn.append(x)
 
-print (len(cn))
-print (img_arr.shape[0], img_arr.shape[1])
+# print (len(cn))
+# print (img_arr.shape[0], img_arr.shape[1])
 
-for i in range (img_arr.shape[0]) :
-    for j in range (img_arr.shape[1]) :
-        img_arr[i][j] = float(img_arr[i][j])
+# for i in range (img_arr.shape[0]) :
+#     for j in range (img_arr.shape[1]) :
+#         img_arr[i][j] = float(img_arr[i][j])
 
-bn = []
+# bn = []
 
-for i in range(256):
-    bn.append(float(i))
+# for i in range(256):
+#     bn.append(float(i))
 
-hist, bin_edges = np.histogram(img_arr, bins=cn)
-bin_centers = 0.5*(bin_edges[:-1] + bin_edges[1:])
+# hist, bin_edges = np.histogram(img_arr, bins=cn)
+# bin_centers = 0.5*(bin_edges[:-1] + bin_edges[1:])
 
 bins_arr = []
+bins_arr1 = []
 bc = []
-bin_size = 30
+bin_size = 10
 
 for i in range(int(255/bin_size) + 1):
 	print(i)
 	bins_arr.append(0)
+	bins_arr1.append(0)
+
 	bc.append(i * bin_size)
 
-print(bins_arr)
+print(bins_arr, len(bins_arr))
 
 for i in range(img_arr.shape[0]):
 	for j in range(img_arr.shape[1]):
@@ -46,6 +49,7 @@ for i in range(img_arr.shape[0]):
 		a = int(img_arr[i][j] / bin_size)
 		b = img_arr[i][j] % bin_size
 		k = abs(0.5 - b / bin_size)
+		# print(img_arr[i][j] , a, b, k)
 		# round(k, 3)
 		# print(k)
 
@@ -60,16 +64,24 @@ for i in range(img_arr.shape[0]):
 			bins_arr[a + 1] += k
 			
 c = 0
-for i in range(int(255/bin_size) + 1):
-	c += bins_arr[i]
 
-print(bins_arr, c)
+for i in range(img_arr.shape[0]):
+	for j in range(img_arr.shape[1]):
+		
+		a = int(img_arr[i][j] / bin_size)
+		bins_arr1[a] += 1
 
+# for i in range(int(255/bin_size) + 1):
+# 	c += bins_arr[i]
+
+
+print(bins_arr, c, len(bins_arr))
 # print (hist)
 
 # 3. Plot the histogram
 # import matplotlib.pyplot as plt
 #plt.plot(bin_centers, hist, lw=2)
 
-plt.plot(bc, bins_arr, lw=2)
+plt.subplot(121),plt.plot(bc, bins_arr, lw=2)
+plt.subplot(122),plt.plot(bc, bins_arr1, lw=2)
 plt.show()
